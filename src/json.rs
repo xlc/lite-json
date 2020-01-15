6ab69617b918c54ec0fa767b2c79a394bc9fa00d
Igor Matuszewski <xanewok@gmail.com>
2020-01-15 11:29:21 +0100

    Add a convenience no_std method to get a f64 out of NumberValue

--- src/json.rs
+++ src/json.rs
@@ -203,9 +203,20 @@ pub struct NumberValue {
     pub exponent: i32,
 }
 
-#[cfg(feature = "std")]
+impl NumberValue {
+    /// Losslessly convert the inner value to `f64`.
+    #[cfg(any(feature = "std", feature = "float"))]
+    pub fn to_f64(self) -> f64 {
+        self.into()
+    }
+}
+
+#[cfg(any(feature = "std", feature = "float"))]
 impl Into<f64> for NumberValue {
     fn into(self) -> f64 {
+        #[cfg(not(feature = "std"))]
+        use num_traits::float::FloatCore as _;
+
         (self.integer as f64 + self.fraction as f64 / 10f64.powi(self.fraction_length as i32))
             * 10f64.powi(self.exponent)
     }
